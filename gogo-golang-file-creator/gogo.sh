7b39c8ca1f6601aef00c2488923583032b24c722
michael@Grothe-IT <mnj@grothe.it>
2025-10-30 14:35:15 +0100

    updating my repo with the newest scripts i wrote

---
+++ gogo-golang-file-creator/gogo.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [[ $# -ne 1 ]]; then
+  echo "Usage: $(basename "$0") <dir>"
+  exit 1
+fi
+
+DIR="$1"
+
+mkdir -p "$DIR"
+pushd "$DIR" >/dev/null
+
+# main.go mit Heredoc erzeugen
+cat > main.go <<'EOF'
+package main
+
+func main () {}
+EOF
+
+go mod init main.go
+go mod tidy
+
+ls
+popd >/dev/null
+
+# öffne mit v wie bei dir
+nvim "./$DIR/main.go"
